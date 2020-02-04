1e7bf93c40ffced67b21b73bfa00eaf09f236f63
SpencerFW <srfalorward@ucdavis.edu>
2020-02-03 21:58:23 -0800

    add the date and time and local storage

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -5,16 +5,27 @@ var formContents = $("<input>", {
   id: "input",
   placeholder: "What's going down?"
 });
+var input = $("#input");
 var save = $(".btn");
+var dateTime = $(".dateTime");
 var todos = [];
 
+// var m = moment (); = date and time now
+var m = moment();
+var mDate = m.format("DD-MM-YYYY");
+var mHour = m.format("HH");
+var timeSlot = $("<h3>");
+timeSlot.text(`${mDate} ${mHour}`);
+dateTime.append(timeSlot);
+console.log(timeSlot);
+
 $(document).ready(function() {
-  cardInput.click(function(event) {
+  cardInput.click(function() {
     // event.preventDefault();
     form.append(formContents);
     form.click(function(event) {
       event.stopPropagation();
-    })
+    });
     $(this).append(form);
   });
 
@@ -28,87 +39,57 @@ $(document).ready(function() {
   save.click(function(event) {
     event.stopPropagation();
     // $(this).append(
-    for (var i = 0; i < todos.length; i++) {
-      var todo = todos[i];
-      var li = $("<li>");
-      li.text(todo);
-      li.attr("data-index", i);
-    }
-    var li = $("<li>");
-    var list = $(this).before(li);
-    $("card-body").wrap(list);
+    // for (var i = 0; i < todos.length; i++) {
+    //   var todo = todos[i];
+    //   var li = $("<li>");
+    //   li.text(todo);
+    //   li.attr("data-index", i);
+    // }
+    // var li = $("<li>");
+    // var list = $(this).before(li);
+    // $("card-body").wrap(list);
     // )
+    storeTodos($(this).data("time"));
+    populateTodos();
   });
 
-  function storeTodos() {
+  function storeTodos(id) {
     // Stringify and set "todos" key in localStorage to todos array
-    localStorage.setItem("todos", JSON.stringify(todos));
+    localStorage.setItem(
+      id,
+      $("input")
+        .val()
+        .trim()
+    );
   }
 
-  save.click(function(event) {
-    event.preventDefault();
-
-    var todoText = todoInput.value.trim();
-
-    // Return from function early if submitted todoText is blank
-    if (todoText === "") {
-      return;
+  function populateTodos() {
+    var timeArray = [8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6];
+    for (var i = 0; i < timeArray.length; i++) {
+      var p = $("<p>").text(localStorage.getItem(`${timeArray[i]}am`));
+      $(`#${timeArray[i]}am`).prepend(p);
     }
-    // Add new todoText to todos array, clear the input
-    todos.push(todoText);
-    todoInput.value = "";
+  }
 
-    // Store updated todos in localStorage, re-render the list
-    storeTodos();
-    renderTodos();
-  });
+  populateTodos();
+  // save.click(function(event) {
+  //   event.preventDefault();
+
+  //   var todoText = input.val().trim();
+
+  //   // Return from function early if submitted todoText is blank
+  //   if (todoText === "") {
+  //     return;
+  //   }
+  //   // Add new todoText to todos array, clear the input
+  //   todos.push(todoText);
+  //   input.val() = "";
+
+  //   // Store updated todos in localStorage, re-render the list
+  //   storeTodos();
+  //   renderTodos();
+  // });
 });
 
-//possible code to target specific click
-// function handler (event) {
-//     var target = $(event.target);
-//     if (target.is("li")) {
-//         target.children().toggle();
-//     }
-// }
-// $("ul").click(handler).find("ul").hide();
-//   cardInput.on("click", function() {
-//     $(".cardTest").append(
-//       // Creating Form Div and Adding <h2> and <p> Paragraph Tag in it.
-//       $("<h3/>").text("Contact Form"),
-//       $("<p/>").text("This is my form. Please fill it out. It's awesome!"),
-//       $("<form/>", {
-//         action: "#",
-//         method: "#"
-//       }).append(
-//         // Create <form> Tag and Appending in HTML Div form1.
-//         $("<input/>", {
-//           type: "text",
-//           id: "vname",
-//           name: "name",
-//           placeholder: "Your Name"
-//         }), // Creating Input Element With Attribute.
-//         $("<input/>", {
-//           type: "text",
-//           id: "vemail",
-//           name: "email",
-//           placeholder: "Your Email"
-//         }),
-//         $("<textarea/>", {
-//           rows: "5px",
-//           cols: "27px",
-//           type: "text",
-//           id: "vmsg",
-//           name: "msg",
-//           placeholder: "Message"
-//         }),
-//         $("<br/>"),
-//         $("<input/>", {
-//           type: "submit",
-//           id: "submit",
-//           value: "Submit"
-//         })
-//       )
-//     );
-//   });
-// });
+// need to set item with local storage and then get item with local storage
+// add bombs and explosion emojis
